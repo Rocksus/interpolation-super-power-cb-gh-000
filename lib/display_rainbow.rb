255d3801b0e3807fe2ccca642495372a2a036b8c
Ray Antonius <rayantonius@gmail.com>
2018-10-28 16:47:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
-define display_rainbow(*c)
+define display_rainbow(c)
   "R: #{c[0]}, O: #{c[1]}, Y: #{c[2]}, G: #{c[3]}, B: #{c[4]}, I: #{c[5]}, V: #{c[6]}"
 end
